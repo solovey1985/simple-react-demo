a490a5905f7e65d3359989da7bf63410729ba42d
solovey1985 <solovey1985@ukr.net>
2019-10-24 18:29:28 +0300

    basic

--- README.md
+++
@@ -1,3 +0,0 @@
-# react-2xccry
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-2xccry)
--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
+function App(){
+  const [counter, setCounter] = useState(42);
+  const incrementCounter = ()=>{ setCounter(counter+1);
 
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Some new staff
-        </p>
-      </div>
-    );
-  }
+  };
+  return (
+    <div>
+      <Button onClickFunction={incrementCounter}/>
+      <Display message={counter}/>
+    </div>
+  );
+}
+
+function Display(props){
+  return(
+    <div>{props.message}</div>
+  );
+}
+function Button(props){
+  return(
+    <button onClick={props.onClickFunction}>+1</button>
+  );
 }
 
 render(<App />, document.getElementById('root'));
