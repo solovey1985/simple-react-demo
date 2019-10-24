602db64ce8de0dc776c50484bd5633abac41c4b7
solovey1985 <solovey1985@ukr.net>
2019-10-24 15:53:16 +0300

    minot changes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class App extends Component {
       <div>
         <Hello name={this.state.name} />
         <p>
-          Start editing to see some magic happen :)
+          Some new staff
         </p>
       </div>
     );
